068497b23c4d84383459624ba7852d57375bc470
hxc9 <alexis.hafner@gmail.com>
2016-10-01 22:36:10 +0200

    Added functional elapsed time timer + timer restar

--- src/c/main.c
+++ src/c/main.c
@@ -12,28 +12,51 @@ static TextLayer *s_main_label;
 static TextLayer *s_main_count;
 
 static Layer *s_endurance_layer;
-static int s_endurance_level = 75;
+static int s_endurance_level = 100;
 
 static TextLayer *s_et_label;
 static TextLayer *s_et_count;
+static TextLayer *s_et_desc;
 
-static void update_time(bool withDate) {
-  time_t temp = time(NULL);
-  struct tm *tick_time_z = gmtime(&temp);
+static time_t s_et_start;
+
+static void update_time(time_t tick) {
+  struct tm *tick_time_z = gmtime(&tick);
   
   static char s_buffer_z[6];
   strftime(s_buffer_z, sizeof(s_buffer_z), "%H%M", tick_time_z);
   text_layer_set_text(s_time_layer_u, s_buffer_z);
   
-  if (withDate) {
-    static char s_buffer_date[11];
-    strftime(s_buffer_date, sizeof(s_buffer_date), "%d", tick_time_z);
-    text_layer_set_text(s_date_layer, s_buffer_date);
-  }
+  static char s_buffer_date[3];
+  strftime(s_buffer_date, sizeof(s_buffer_date), "%d", tick_time_z);
+  text_layer_set_text(s_date_layer, s_buffer_date);
+}
+
+static void update_elapsed_time(time_t tick) {
+  int elapsed_time = tick - s_et_start;
+  int seconds = (int)elapsed_time % 60;
+  int minutes = (int)elapsed_time / 60 % 100;
+  
+  static char et_buffer[] = "00:00";
+  snprintf(et_buffer, sizeof(et_buffer), "%d:%02d", minutes, seconds);
+  text_layer_set_text(s_et_count, et_buffer);
+}
+
+static void start_elapsed_time() {
+  s_et_start = time(NULL);
+  struct tm *tick_time_z = gmtime(&s_et_start);
+  
+  static char et_start_buffer[] = "xx";
+  snprintf(et_start_buffer, sizeof(et_start_buffer), "%02d", tick_time_z->tm_min);
+  text_layer_set_text(s_et_label, et_start_buffer);
+  
+  update_elapsed_time(s_et_start);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
- update_time(false);
+  time_t tick = time(NULL);
+  update_time(tick);
+  update_elapsed_time(tick);
 }
 
 static void battery_update_proc(Layer *layer, GContext *ctx) {
@@ -64,12 +87,20 @@ static TextLayer *configure_text_layer(Layer *window_layer, GRect box, GFont fon
   
   text_layer_set_background_color(layer, GColorClear);
   text_layer_set_text_color(layer, GColorWhite);
-  text_layer_set_font(layer, font); // todo check other fonts
+  text_layer_set_font(layer, font);
   text_layer_set_text_alignment(layer, alignment);
   
   layer_add_child(window_layer, text_layer_get_layer(layer));
   
   return layer;
+}
+
+static void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  start_elapsed_time();
+}
+
+static void config_provider(Window *window) {
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
 }
 
 static void main_window_load(Window *window) {
@@ -95,8 +126,8 @@ static void main_window_load(Window *window) {
   
   s_et_label = configure_text_layer(window_layer, GRect(0, 115, bounds.size.w, 44), fonts_get_system_font(FONT_KEY_BITHAM_34_MEDIUM_NUMBERS), GTextAlignmentLeft);
   s_et_count = configure_text_layer(window_layer, GRect(0, 115, bounds.size.w, 44), fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS), GTextAlignmentRight);
-  text_layer_set_text(s_et_label, "88");
-  text_layer_set_text(s_et_count, "88:88");
+  s_et_desc = configure_text_layer(window_layer, GRect(10, bounds.size.h - 26, 20, 26), fonts_get_system_font(FONT_KEY_GOTHIC_24), GTextAlignmentLeft);
+  text_layer_set_text(s_et_desc, "ET");
 }
 
 static void main_window_unload(Window *window) {
@@ -118,11 +149,14 @@ static void init() {
   });
   window_stack_push(s_main_window, true);
   
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
   battery_state_service_subscribe(battery_callback);
+  
+  window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
   
   update_time(true);
   battery_callback(battery_state_service_peek());
+  start_elapsed_time();
 }
 
 static void deinit() {
